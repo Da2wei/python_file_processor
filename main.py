184e167f823fba150d96d491f90cabc5e17b5cdc
Da2wei <141890261+Da2wei@users.noreply.github.com>
2023-08-10 14:30:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import file_operations
 
 def main():
     # TODO: 添加用戶界面和主要的執行邏輯
+    # 測試代碼
+    #寫入文件
+    file_operations.write_file('test.txt', 'Hello, GitHub!')
+
+    # 讀取文件
+    content = file_operations.read_file('test.txt')
+    print(content)  # 應該輸出 'Hello, GitHub!'
     pass
 
 if __name__ == "__main__":
