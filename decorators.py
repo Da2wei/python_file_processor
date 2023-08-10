45fce1f63d6ad378326be4d791724e6de37a4637
Da2wei <141890261+Da2wei@users.noreply.github.com>
2023-08-10 15:30:12 +0800

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,13 +1,35 @@
+# decorators.py
+
 import time
 
 def timer_decorator(func):
+    """
+    裝飾器用於測量函數的執行時間。
+
+    參數:
+    - func: 被裝飾的函數
+
+    返回:
+    - wrapper: 修改後的函數
+    """
     def wrapper(*args, **kwargs):
+        # 記錄函數開始執行的時間
         start_time = time.time()
+        
+        # 調用被裝飾的函數並獲取結果
         result = func(*args, **kwargs)
+        
+        # 記錄函數結束執行的時間
         end_time = time.time()
-        print(f"{func.__name__} took {end_time - start_time:.2f} seconds to run.")
+        
+        # 計算並打印函數的執行時間
+        print(f"{func.__name__} took {end_time - start_time} seconds to run.")
+        
+        # 返回被裝飾的函數的結果
         return result
+
     return wrapper
+
 
 def log_decorator(func):
     def wrapper(*args, **kwargs):
