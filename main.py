a05e43ec42440d21447dc69cc0dffeed93fb938f
Da2wei <141890261+Da2wei@users.noreply.github.com>
2023-08-10 14:45:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import argparse
 import file_operations
 
 def main():
-    # TODO: 添加用戶界面和主要的執行邏輯
-    # 測試代碼
-    #寫入文件
-    file_operations.write_file('test.txt', 'Hello, GitHub!')
-
-    # 讀取文件
-    content = file_operations.read_file('test.txt')
-    print(content)  # 應該輸出 'Hello, GitHub!'
+    parser = argparse.ArgumentParser(description="Python File Processor")
     
+    # 定義命令行參數
+    parser.add_argument("-r", "--read", help="Read a file", action="store", dest="read_file")
+    parser.add_argument("-w", "--write", help="Write to a file", action="store", dest="write_file")
+    parser.add_argument("-c", "--content", help="Content to write", action="store", dest="content")
+    
+    # 解析命令行參數
+    args = parser.parse_args()
+
+    # 執行對應的操作
+    if args.read_file:
+        print(file_operations.read_file(args.read_file))
+    elif args.write_file and args.content:
+        file_operations.write_file(args.write_file, args.content)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
